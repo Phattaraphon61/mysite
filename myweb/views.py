dc37534918baa492d31b43be524c4dd080d7e79a
Phattaraphon61 <phattaraphon.pe.61@ubu.ac.th>
2020-03-18 20:39:01 +0700

    selectdata

--- myweb/views.py
+++ myweb/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import problem , problem_select
+from datetime import date
 
 # Create your views here.
+def mainpage(req):
+    data = problem.objects.all()
+    return render(req,'index.html',{'datas': data})
+
+def selectdata(req):
+    data = req.POST
+    for i in data :
+        if (i != "csrfmiddlewaretoken"):
+            problem_select.objects.create(
+            problem_name = i,
+            username_select = "amd",
+            problem_date_select = date.today())
+        
+
+    
+
